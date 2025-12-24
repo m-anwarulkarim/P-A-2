67f9dd1fe14026c5c99c73af107276ac9d31fe74
anwar <dev.anwarul@gmail.com>
2025-12-24 10:37:46 +0600

    updated

--- dist/modules/vehicles/vehicles.route.js
+++ dist/modules/vehicles/vehicles.route.js
@@ -4,8 +4,8 @@ import auth from "../../middleware/auth.js";
 import { role } from "../../utils/role.js";
 const router = Router();
 router.post("/", auth(role.admin), vehiclesController.createVehicles);
-router.get("/", auth(role.admin, role.customer), vehiclesController.getAllVehicles);
-router.get("/:id", auth(role.admin, role.customer), vehiclesController.getSingelVehicles);
+router.get("/", vehiclesController.getAllVehicles);
+router.get("/:id", vehiclesController.getSingelVehicles);
 router.put("/:id", auth(role.admin), vehiclesController.updateVehicles); //
 router.delete("/:id", auth(role.admin), vehiclesController.deleteVehicles); //
 export const vehiclesRouter = router;
